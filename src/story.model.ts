c9e5d82889ba834bac9f644d4121a9d8041549ae
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-03-30 08:56:46 -0700

    add story model

---
+++ src/story.model.ts
@@ -0,0 +1,5 @@
+export class Story{
+
+  constructor(public title: string, public imgUrl: string, public author: string, public source: string, public category: string, public date: Date, public except: string) { }
+
+}
